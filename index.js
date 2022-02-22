ee5107ef27bb277f52611e4c5ef0d038d761091a
pandev9329 <99141349+pandev9329@users.noreply.github.com>
2022-02-21 21:49:30 -0800

    Add files via upload
    update index.js

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ var bodyparser = require('body-parser');
 var prevData = require("./prevData.json");
 
 const APIURL = 'https://api.thegraph.com/subgraphs/name/hch86702/phoenix';
+const NFTAPIURL = 'https://api.thegraph.com/subgraphs/name/david86702/phoenix-new';
 
+const query1 = `{
+	purchasedNFTs(first: 1000, skip: 0, orderBy: createTime, orderDirection: asc, where: {createTime_gt: 0}) {
+	  addr
+	  typeOfNFT
+	  createTime
+	}
+  }`;
+  
 
 
 dbo.connectToServer(function (err) {
@@ -36,6 +45,15 @@ dbo.connectToServer(function (err) {
 			res.json({ data: list });
 		});
 
+		app.post("/get_nft_count", async function(req, res) {
+			var list = await db.collection('nfts').aggregate([
+				{$group: {_id: "$typeOfNFT", total: {$sum: 1}}}
+			]).toArray();
+			res.json({data: list});
+		})
+
+
+
 		async function setPrevData() {
 			var temp = [];
 			prevData.forEach(element => {
@@ -51,15 +69,17 @@ dbo.connectToServer(function (err) {
 			}
 		}
 		setPrevData();
-
 		setInterval(() => {
 			getData(db.collection('nests'));
+			getNftdata(db.collection('nfts'));
 		}, 100000);
+
 
 		app.listen(process.env.PORT || 4000, () => console.log('Listening on port 4000'));
 	}
-
 });
+
+
 
 
 async function getData(collection) {
@@ -86,4 +106,32 @@ async function getData(collection) {
 		});
 }
 
-
+async function getNftdata(collection) {
+	var list = await collection.find().sort({createTime: -1}).limit(1).toArray();
+	var lastCreateTime = '10000';
+	if (list.length != 0) {
+		lastCreateTime = list[0].createTime;
+	}
+	const query = {
+		query: 
+		`{
+			purchasedNFTs(first: 1000, skip: 0, orderBy: createTime, orderDirection: asc, where: {createTime_gt: ${lastCreateTime}}) {
+			  addr
+			  typeOfNFT
+			  createTime
+			}
+		  }`,
+		variables: {}
+	};
+	
+	axios.post(NFTAPIURL, query)
+	.then(async function (response) {
+		if (response.data.data.purchasedNFTs && response.data.data.purchasedNFTs.length > 0) {
+			const insertResult = await collection.insertMany(response.data.data.purchasedNFTs);
+			console.log('Inserted documents =>', insertResult);
+		}
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+}
